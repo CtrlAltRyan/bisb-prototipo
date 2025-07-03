db97f9d4812199c019291148dca1ee7d43167095
Rodrigo Sousa Barbosa <108591857+rodrigosousa13@users.noreply.github.com>
2025-07-03 01:51:51 -0300

    Implementada exibição das tabelas na db, adição do gitignore

--- routes/user.py
+++ routes/user.py
@@ -1,12 +1,25 @@
 from flask import Blueprint, render_template, session, redirect, url_for, flash
 from flask_login import login_required
 from .info import get_topservice, get_amount, get_customersnum
+from connection import get_db_connection
 
 user_bp = Blueprint('user', __name__, template_folder='templates')
 
 @user_bp.route('/customers')
 def customers_page():
-    return render_template('customers.html')
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    cur.execute("""
+        SELECT *
+        FROM salao.clientes;
+    """)
+    clientes = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return render_template('customers.html', clientes=clientes)
 
 @user_bp.route('/home')
 def home_page():
@@ -18,11 +31,35 @@ def home_page():
 
 @user_bp.route('/services')
 def services_page():
-    return render_template('services.html')
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    cur.execute("""
+        SELECT *
+        FROM salao.servicos;
+    """)
+    services = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return render_template('services.html', services=services)
 
 @user_bp.route('/sales')
 def sales_page():
-    return render_template('sales.html')
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    cur.execute("""
+        SELECT id_cliente, id_servico, valor, forma_pagamento, data_venda
+        FROM salao.vendas;
+    """)
+    sales = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return render_template('sales.html', sales=sales)
 
 @user_bp.before_request
 def check_authentication():
