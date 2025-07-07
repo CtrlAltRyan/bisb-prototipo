f0e3356a1e592ccb82e50683788ac1eb103c9384
Rodrigo Sousa Barbosa <108591857+rodrigosousa13@users.noreply.github.com>
2025-07-07 09:02:28 -0300

    Adicionado .env.example para uso do connection.py

--- connection.py
+++ connection.py
@@ -1,10 +1,12 @@
 import psycopg2
+import os
 
-# Configuração da conexão com PostgreSQL
+
+# Configuração da conexão com PostgreSQL usando variáveis de ambiente
 def get_db_connection():
     return psycopg2.connect(
-        dbname="bisbdb",
-        user="postgres",
-        password="ryanboy2",
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
         host="localhost"
     )
