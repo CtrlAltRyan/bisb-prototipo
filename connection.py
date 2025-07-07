5e160fc0508161cd872bc51db3995caa5756ca40
Rodrigo Sousa Barbosa <108591857+rodrigosousa13@users.noreply.github.com>
2025-07-07 09:58:26 -0300

    Ajeitando o connection.py para funcionar

--- connection.py
+++ connection.py
@@ -1,7 +1,8 @@
+from dotenv import load_dotenv
 import psycopg2
 import os
 
-
+load_dotenv()
 # Configuração da conexão com PostgreSQL usando variáveis de ambiente
 def get_db_connection():
     return psycopg2.connect(
